b250379cbb83ad151a27f27bb168d2b42155a5a3
Piper Dougherty <doughertypiper@gmail.com>
2021-11-10 17:56:36 -0600

    Should be lavender_bison.

--- firewall.tf
+++ firewall.tf
@@ -1,6 +1,6 @@
 resource "google_compute_firewall" "allow_packer_build_machine_ssh" {
   name    = "allow-packer-build-machine-ssh"
-  network = google_compute_network.general.name
+  network = google_compute_network.lavender_bison.name
 
   allow {
     protocol = "tcp"
